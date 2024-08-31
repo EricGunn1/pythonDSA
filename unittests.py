f42ea25d8f49be14b2ac28eb3e2c29cf16015c49
Eric <eng0130@gmail.com>
2024-08-30 21:13:39 -0400

    unit tests

---
+++ unittests.py
@@ -0,0 +1,21 @@
+import lists
+
+
+class UnitTests:
+    inData = 13
+
+    @staticmethod
+    def calculateNodeData(intData, numberOfNodes):
+        return intData * (numberOfNodes - 1)
+
+    def testPushStack(self):
+        pass
+
+    def testPopStack(self):
+        pass
+
+    def testEnQueue(self):
+        pass
+
+    def testDeQueue(self):
+        pass
